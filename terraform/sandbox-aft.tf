b418e50dfae2047bd0a62bcb2fcb19ce63aa5d93
Shaun Bradley <shaun.bradley@checkit.net>
2022-02-21 14:32:35 +0000

    sandbox acct

---
+++ terraform/sandbox-aft.tf
@@ -0,0 +1,27 @@
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "kernow5000@gmail.com"
+    AccountName               = "sandbox-aft"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "shaun.bradley@checkit.net"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Still testing I'm afraid"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
