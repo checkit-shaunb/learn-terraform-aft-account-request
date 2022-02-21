f2432b1215ae213bcd6e086005a66a7335ec0d94
Shaun Bradley <shaun.bradley@checkit.net>
2022-02-21 12:44:13 +0000

    create prod account testing

---
+++ terraform/prod.tf
@@ -0,0 +1,27 @@
+module "prod" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws_root_prod@checkit.net"
+    AccountName               = "Prod-Account"
+    ManagedOrganizationalUnit = "LearnAFT"
+    SSOUserEmail              = "shaun.bradley@checkit.net"
+    SSOUserFirstName          = "Prod"
+    SSOUserLastName           = "Account"
+  }
+
+  account_tags = {
+    "Environment" = "Prod"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Shaun Bradley"
+    change_reason       = "Initial AWS account creation"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "Prod"
+}
