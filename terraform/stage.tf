b2642965a41858fa2ac5d44f1f4ef2cbca8b42e2
Shaun Bradley <shaun.bradley@checkit.net>
2022-02-21 10:28:29 +0000

    create stage acct under LearnAFT OU

---
+++ terraform/stage.tf
@@ -0,0 +1,27 @@
+module "stage" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws_root_stage@checkit.net"
+    AccountName               = "Stage-Account"
+    ManagedOrganizationalUnit = "LearnAFT"
+    SSOUserEmail              = "shaun.bradley@checkit.net"
+    SSOUserFirstName          = "Stage"
+    SSOUserLastName           = "Account"
+  }
+
+  account_tags = {
+    "Environment" = "Stage"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Shaun Bradley"
+    change_reason       = "Initial AWS account creation"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "stage"
+}
