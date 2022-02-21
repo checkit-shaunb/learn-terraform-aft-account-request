bd9153af9dfbb48f270a6ddfb4fe98307513f37e
Shaun Bradley <shaun.bradley@checkit.net>
2022-02-21 09:46:46 +0000

    added a test account to check limits/creation

---
+++ terraform/test.tf
@@ -0,0 +1,28 @@
+module "shaunplayground" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "kernow5000@gmail.com"
+    AccountName               = "kernorgtest"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "shaun.bradley@checkit.net"
+    SSOUserFirstName          = "kern"
+    SSOUserLastName           = "orgtest"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+    "Creator" = "Shaun Bradley"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Shaun Bradley"
+    change_reason       = "Working out acct deletion and state"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "shaunplayground"
+}
